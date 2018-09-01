5a19c8f4018cc9c6e001a4eeb83241b423a89940
NEMESIS\Hattackk <matthewsaiken@gmail.com>
2018-09-01 17:31:20 -0400

    Started working on the hit control for the crosshair

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -1,8 +1,21 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankPlayerController.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
 #include "BattleTank.h"
 
+
+//Tick
+//Super
+// AimeTowardsCrosshair();
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+	
+
+}
 void ATankPlayerController::BeginPlay() {
 	Super::BeginPlay();
 
@@ -17,6 +30,29 @@ void ATankPlayerController::BeginPlay() {
 
 ATank* ATankPlayerController::GetControlledTank() const {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair() {
+	if (!GetControlledTank()) {return;}
+
+	FVector HitLocation;
+	if (GetSightRayHitLocation(HitLocation)) {
+		UE_LOG(LogTemp, Warning, TEXT("Hit Location: %s"),*HitLocation.ToString());
+
+		//Get world location of linetrace through crosshair
+		// if it hits the landscape
+		//tell controlled tank to aim at this point
+
+	}
+
+
+}
+bool ATankPlayerController::GetSightRayHitLocation(FVector &Hitlocation) const{
+	//GetWorld()->GetFirstPlayerController()->
+	Hitlocation = FVector(1.0);
+	return true;
+
+
 }
 
 
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -16,13 +16,39 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-	ATank * GetControlledTank() const;
-
+	virtual void Tick(float DeltaTime) override;
 	virtual void BeginPlay() override;
 
+	/*************************************************/
+	/** GetControlledTank();                        **/
+	/*************************************************/
+	/* Returns a pointer to the tank being           */
+	/* controlled by the player                      */
+	/*************************************************/
+
+	ATank * GetControlledTank() const;
+
+	/*************************************************/
+	/** AimTowardsCrosshair();                      **/
+	/*************************************************/
+	/* Start the tank moving the barrel so that a    */
+	/* shot would hit where the crosshairs are       */
+	/* pointing.                                     */
+	/*************************************************/
+
+	void AimTowardsCrosshair();
 
 
 	
+
+private:
+	/*************************************************/
+	/** GetSightRayHitLocation();                   **/
+	/*************************************************/
+	/* Find what the line trace hits                 */
+	/*************************************************/
+
+	bool GetSightRayHitLocation(FVector& HitLocation) const;
 	
 	
 	
